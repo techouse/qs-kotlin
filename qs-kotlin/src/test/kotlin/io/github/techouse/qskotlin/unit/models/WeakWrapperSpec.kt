c3332c7c977f54fdffb9db0e63b67cfe6ac5e030
Klemen Tušar <techouse@gmail.com>
2025-08-02 11:22:11 +0100

    :white_check_mark: add test for WeakWrapper toString() behavior after garbage collection

--- qs-kotlin/src/test/kotlin/io/github/techouse/qskotlin/unit/models/WeakWrapperSpec.kt
+++ qs-kotlin/src/test/kotlin/io/github/techouse/qskotlin/unit/models/WeakWrapperSpec.kt
@@ -25,5 +25,28 @@ class WeakWrapperSpec :
                 val w = WeakWrapper(o)
                 w.toString() shouldContain "WeakWrapper("
             }
+
+            it("toString shows <collected> when referent is garbage collected") {
+                var obj: Any? = Any()
+                val wrapper = WeakWrapper(obj!!)
+
+                // Clear the reference and force garbage collection
+                obj = null
+                System.gc()
+                System.runFinalization()
+
+                // The referent might still be alive, so we need to be more aggressive
+                for (i in 1..10) {
+                    System.gc()
+                    System.runFinalization()
+                    Thread.sleep(10)
+                    if (wrapper.get() == null) break
+                }
+
+                // If GC was successful, toString should show <collected>
+                if (wrapper.get() == null) {
+                    wrapper.toString() shouldBe "WeakWrapper(<collected>)"
+                }
+            }
         }
     })
