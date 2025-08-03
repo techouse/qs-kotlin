03742a3664669080c2d428bac3f25d9c240e2f62
Klemen Tusar <techouse@gmail.com>
2025-08-03 11:13:40 +0100

    :hammer: add Dokka plugin for documentation generation in subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("android") version "2.0.21" apply false
     id("com.android.library") version "8.10.1" apply false
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+    id("org.jetbrains.dokka") version "1.9.20" apply false
 }
 
 allprojects {
@@ -12,6 +13,11 @@ allprojects {
         google()
         mavenCentral()
     }
+}
+
+subprojects {
+    plugins.withId("org.jetbrains.kotlin.jvm") { apply(plugin = "org.jetbrains.dokka") }
+    plugins.withId("org.jetbrains.kotlin.android") { apply(plugin = "org.jetbrains.dokka") }
 }
 
 nexusPublishing {
