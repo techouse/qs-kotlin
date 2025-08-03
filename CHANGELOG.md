206e35b1f73da4422db2766787e481e4e0e6c1e3
Klemen Tusar <techouse@gmail.com>
2025-08-03 10:02:56 +0100

    :bookmark: release v1.0.1
    ## 1.0.1
    
    * [CHORE] Update Kotlin version to 2.0.21.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.1
+
+* [CHORE] Update Kotlin version to 2.0.21.
+
 ## 1.0.0
 
 * [CHORE] Initial release of the project.
--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 allprojects {
     group = "io.github.techouse"
-    version = "1.0.0"
+    version = "1.0.1"
     repositories {
         google()
         mavenCentral()
