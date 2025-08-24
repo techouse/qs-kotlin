b0cf738f824dcf5baf30bbad30961bebe5ea49a4
Klemen Tusar <techouse@gmail.com>
2025-08-24 08:10:18 +0100

    :hammer: improve build reproducibility and enhance publication metadata

--- qs-kotlin/build.gradle.kts
+++ qs-kotlin/build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.jvm.tasks.Jar
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -17,6 +18,11 @@ java {
 }
 
 jacoco { toolVersion = "0.8.13" }
+
+tasks.withType<Jar>().configureEach {
+    isPreserveFileTimestamps = false
+    isReproducibleFileOrder = true
+}
 
 tasks.withType<KotlinCompile> {
     compilerOptions.apply {
@@ -63,17 +69,28 @@ publishing {
                 description.set(
                     "A query string encoding and decoding library for Android and Kotlin/JVM. Ported from qs for JavaScript."
                 )
-                url.set("https://github.com/techouse/qs-kotlin")
+                url.set("https://techouse.github.io/qs-kotlin/")
+                inceptionYear.set("2025")
                 licenses {
                     license {
-                        name.set("BSD-3-Clause License")
-                        url.set("https://opensource.org/license/bsd-3-clause")
+                        name.set("BSD-3-Clause")
+                        url.set("https://github.com/techouse/qs-kotlin/blob/main/LICENSE")
+                        distribution.set("repo")
                     }
                 }
                 scm {
                     connection.set("scm:git:https://github.com/techouse/qs-kotlin.git")
                     developerConnection.set("scm:git:ssh://git@github.com/techouse/qs-kotlin.git")
                     url.set("https://github.com/techouse/qs-kotlin")
+                    tag.set("HEAD")
+                }
+                issueManagement {
+                    system.set("GitHub Issues")
+                    url.set("https://github.com/techouse/qs-kotlin/issues")
+                }
+                ciManagement {
+                    system.set("GitHub Actions")
+                    url.set("https://github.com/techouse/qs-kotlin/actions")
                 }
                 developers {
                     developer {
@@ -81,17 +98,31 @@ publishing {
                         name.set("Klemen Tusar")
                         email.set("techouse@gmail.com")
                         url.set("https://github.com/techouse")
+                        roles.set(listOf("Lead", "Maintainer"))
+                        timezone.set("Europe/London")
+                        properties.put("twitter", "https://x.com/nextk2")
+                        properties.put("linkedin", "https://www.linkedin.com/in/techouse/")
+                        properties.put("sponsor", "https://github.com/sponsors/techouse")
+                        properties.put("paypal", "https://paypal.me/ktusar")
                     }
                 }
+                properties.put(
+                    "changelogUrl",
+                    "https://github.com/techouse/qs_codec/blob/master/CHANGELOG.md",
+                )
             }
         }
     }
 }
 
 signing {
-    useInMemoryPgpKeys(
-        providers.gradleProperty("signingInMemoryKey").getOrElse(""),
-        providers.gradleProperty("signingInMemoryKeyPassword").getOrElse(""),
-    )
-    sign(publishing.publications)
+    val hasKey = providers.gradleProperty("signingInMemoryKey").isPresent
+    isRequired = hasKey && !project.version.toString().endsWith("SNAPSHOT")
+    if (hasKey) {
+        useInMemoryPgpKeys(
+            providers.gradleProperty("signingInMemoryKey").get(),
+            providers.gradleProperty("signingInMemoryKeyPassword").getOrElse(""),
+        )
+        sign(publishing.publications)
+    }
 }
