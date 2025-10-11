da8281ca58400551c1e82a7458f091e8ec95ffba
Klemen Tusar <techouse@gmail.com>
2025-10-11 11:35:54 +0100

    :white_check_mark: enhance test coverage for encoding and decoding functions

--- qs-kotlin/src/test/kotlin/io/github/techouse/qskotlin/unit/ExtensionsSpec.kt
+++ qs-kotlin/src/test/kotlin/io/github/techouse/qskotlin/unit/ExtensionsSpec.kt
@@ -1,32 +1,21 @@
 package io.github.techouse.qskotlin.unit
 
-import io.github.techouse.qskotlin.fixtures.data.EndToEndTestCases
 import io.github.techouse.qskotlin.models.EncodeOptions
 import io.github.techouse.qskotlin.toQueryMap
 import io.github.techouse.qskotlin.toQueryString
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class ExtensionsSpec :
-    FunSpec({
-        context("String.toQueryMap") {
-            EndToEndTestCases.forEach { testCase ->
-                test("should decode: ${testCase.encoded}") {
-                    testCase.encoded.toQueryMap() shouldBe testCase.data
-                }
-            }
-        }
-
-        context("Map.toQueryString") {
-            EndToEndTestCases.forEach { testCase ->
-                test("should encode: ${testCase.data}") {
-                    testCase.data.toQueryString(EncodeOptions(encode = false)) shouldBe
-                        testCase.encoded
-                }
+    DescribeSpec({
+        describe("Kotlin extension wrappers") {
+            it("String.toQueryMap delegates to decode") {
+                "a=1&b=2".toQueryMap() shouldBe mapOf("a" to "1", "b" to "2")
             }
 
-            test("uses default EncodeOptions overload when omitted") {
-                mapOf("a" to "b").toQueryString() shouldBe "a=b"
+            it("Map.toQueryString delegates to encode") {
+                linkedMapOf("a" to 1, "b" to 2)
+                    .toQueryString(EncodeOptions(encode = false)) shouldBe "a=1&b=2"
             }
         }
     })
